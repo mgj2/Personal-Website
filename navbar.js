fa7074b1022e5b72c1e54b137e4366ef3c10238f
Miles Johnson <mgj2@cecs.pdx.edu>
2022-06-02 17:31:27 -0700

    fixed contact form

--- navbar.js
+++ navbar.js
@@ -1,3 +1,21 @@
+window.onscroll = function () {
+  scrollFunction();
+};
+
+// Code adapted from https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+function scrollFunction() {
+  if (window.pageYOffset > 100) {
+    document.getElementById("scrollButton").style.display = "block";
+  } else {
+    document.getElementById("scrollButton").style.display = "none";
+  }
+}
+
+// Code adapted from https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+function scrollToTop() {
+  document.documentElement.scrollTop = 0;
+}
+
 // Menu code adapted from Cem Eygi Media on Youtube:
 // https://www.youtube.com/watch?v=mh36Kts-wX8
 
