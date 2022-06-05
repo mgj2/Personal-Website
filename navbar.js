3b835f40306c8d66a50173cd61fb79b09a73ccf8
Miles Johnson <mgj2@cecs.pdx.edu>
2022-06-05 14:13:50 -0700

    Fixed bug where navbar would be hidden when resizing window from mobile-width

--- navbar.js
+++ navbar.js
@@ -23,7 +23,25 @@ const menu_button = document.getElementById(`menu`);
 const nav_list = document.getElementById(`nav_list`);
 
 menu_button.addEventListener(`click`, () => {
-  if (nav_list.style.display === `none`) {
+  if (nav_list.style.display === `none` || !nav_list.style.display) {
+    nav_list.style.display = `flex`;
+  } else {
+    nav_list.style.display = `none`;
+  }
+});
+
+const nav_links = document.getElementsByClassName(`nav_link`);
+
+for (var i = 0; i < nav_links.length; i++) {
+  nav_links[i].addEventListener(`click`, () => {
+    if (window.innerWidth <= 480) {
+      nav_list.style.display = `none`;
+    }
+  });
+}
+
+window.addEventListener(`resize`, () => {
+  if (window.innerWidth > 480) {
     nav_list.style.display = `flex`;
   } else {
     nav_list.style.display = `none`;
